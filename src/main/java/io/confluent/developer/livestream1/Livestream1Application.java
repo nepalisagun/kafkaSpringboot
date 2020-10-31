b4753e2d8c79143e0fe2c765de949e1b88c3a324
Sagun <sshrestha.me@gmail.com>
2020-10-30 20:42:53 -0400

    Init

---
+++ src/main/java/io/confluent/developer/livestream1/Livestream1Application.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.livestream1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Livestream1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Livestream1Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/confluent/developer/livestream1/Livestream1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.livestream1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Livestream1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
